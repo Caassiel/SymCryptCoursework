f1605c48dd1c377651e1bd82e024dbce53b3fe80
Caassiel <145002684+Caassiel@users.noreply.github.com>
2026-04-25 15:29:13 +0300

    23

--- functions.hpp
+++ functions.hpp
@@ -14,12 +14,12 @@ public:
 
 	static uint32_t G_function(uint32_t X);
 	static uint64_t rotate_left(uint64_t x, unsigned int k);
-    	static uint64_t rotate_right(uint64_t x, unsigned int k);
+    static uint64_t rotate_right(uint64_t x, unsigned int k);
 	void Generate_Round_Keys();
    	
 	static uint64_t F_function(uint64_t half, const SEED& key, int round);
 
-    	SEED Encrypt_Block(const SEED& block, const SEED& key);
+    SEED Encrypt_Block(const SEED& block, const SEED& key);
 	SEED Decrypt_Block(const SEED& block, const SEED& key);
 	
    	void Unit_Test();
